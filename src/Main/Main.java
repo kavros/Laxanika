c0c1a19f2ed9db75448bee6e3252d9b55a5b2b91
kavros <akavroulakis@gmail.com>
2018-01-25 17:39:46 +0200

    main updated with new pdfParser class readme updated

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,14 +5,24 @@ import Model.*;
 public class Main {
     public static  void main(String[] args) {
 
-        ExcelReader reader = new ExcelReader();
+        /*ExcelReader reader = new ExcelReader();
         VFHashMap map    = new VFHashMap();
         VFVector data = new VFVector();
         reader.readExcelFile("C:\\Users\\Alexis\\Desktop\\misc\\laxanika\\a2.xls", data);
         data.update(map);
+*/
+        try{
 
-        VFKef5DataBase dataBase = new VFKef5DataBase();
-        dataBase.testDatabase();
+            VFVector vector = new VFVector();
+            PdfParser reader = new PdfParser();
+            reader.parsePdfFile("C:\\Users\\Alexis\\IdeaProjects\\Laxanika\\pdf\\a.pdf",vector);
+            System.out.println(vector);
+            VFKef5DataBase dataBase = new VFKef5DataBase();
+            dataBase.testDatabase();
+
+        }catch (Exception e){
+            System.out.println(e.getStackTrace());
+        }
 
 
     }
